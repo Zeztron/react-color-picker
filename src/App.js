98f6412998829cd30f0a33d14afc564b91be4a79
Zeztron <pateltharsh@gmail.com>
2019-04-26 12:48:44 -0400

    added react router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Palette from './Palette';
 import seedColors from './seedColor';
 import { generatePalette } from './colorHelpers';
@@ -6,9 +7,13 @@ class App extends Component {
     render() {
       // console.log(generatePalette(seedColors[4]));
       return (
-        <div>
-          <Palette palette={generatePalette(seedColors[4])}/>
-        </div>
+        <Switch>
+          <Route exact path="/" render={() => <h1>Paletee list</h1>}/>
+          <Route exact path="/palette/:id" render={() => <h1>Individual palette</h1>}/>
+        </Switch>
+        // <div>
+        //   <Palette palette={generatePalette(seedColors[4])}/>
+        // </div>
       );
     }
   }
