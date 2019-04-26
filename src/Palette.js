198626a869a127d529ccf28b88ca3b4b3f9632db
Zeztron <pateltharsh@gmail.com>
2019-04-26 11:50:14 -0400

    added material ui and selects

--- src/Palette.js
+++ src/Palette.js
@@ -6,9 +6,11 @@ class Palette extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            level: 500
+            level: 500,
+            format: 'hex'
         }
         this.changeLevel = this.changeLevel.bind(this);
+        this.changeFormat = this.changeFormat.bind(this);
     }
 
     changeLevel = (newLevel) => {
@@ -16,17 +18,22 @@ class Palette extends Component {
             level: newLevel
         })
     }
+
+    changeFormat = (val) => {
+        this.setState({ format: val });
+    }
     
     render() {
         const { colors } = this.props.palette;
-        const { level } = this.state;
+        const { level, format } = this.state;
+
         return (
             <div className="Palette">
-                <Navbar level={level} changeLevel={this.changeLevel}/>
+                <Navbar level={level} changeLevel={this.changeLevel} handleChange={this.changeFormat}/>
             {/* Navbar goes here */}
                 <div className="Palette-colors">
                     {colors[level].map(color => (
-                        <ColorBox key={color.id} background={color.hex} name={color.name}/>
+                        <ColorBox key={color.id} background={color[format]} name={color.name}/>
                     ))}
                 </div>
                 {/* footer */}
