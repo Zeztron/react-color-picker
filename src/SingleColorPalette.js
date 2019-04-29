8b89329b45cdd8e607e2a991ee6460a7a87057d1
Zeztron <pateltharsh@gmail.com>
2019-04-29 14:18:22 -0400

    moved styles into new folder

--- src/SingleColorPalette.js
+++ src/SingleColorPalette.js
@@ -4,48 +4,8 @@ import { withStyles } from '@material-ui/styles';
 import ColorBox from './ColorBox';
 import Navbar from './Navbar';
 import PaletteFooter from './PaletteFooter';
+import styles from './styles/PaletteStyles';
 
-const styles = {
-    Palette: {
-        height: "100vh",
-        display: "flex",
-        flexDirection: "column"
-    },
-    colors: {
-        height: "90%"
-    },
-    goBack: {
-        width: "20%",
-        height: "50%",
-        margin: "0 auto",
-        display: "inline-block",
-        position: "relative",
-        cursor: "pointer",
-        marginBottom: "-3.5px",
-        opacity: "1",
-        backgroundColor: "black",
-        "& a": {
-            color: "#fff",
-            width: "100px",
-            height: "30px",
-            position: "absolute",
-            display: "inline-block",
-            top: "50%",
-            left: "50%",
-            marginLeft: "-50px",
-            marginTop: "-15px",
-            textAlign: "center",
-            outline: "none",
-            background: "rgba(255, 255, 255, .3)",
-            fontSize: "1rem",
-            lineHeight: "30px",
-            textTransform: "uppercase",
-            border: "none",
-            textDecoration: "none",
-            transition: "0.5s"
-        }
-    }
-}
 
 class SingleColorPalette extends Component {
     constructor(props) {
