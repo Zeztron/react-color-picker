ad79a9585010b5f8e90cb141bb8a4709c8e4090d
Zeztron <pateltharsh@gmail.com>
2019-05-03 12:37:22 -0400

    added color box delete

--- src/DraggableColorBox.js
+++ src/DraggableColorBox.js
@@ -37,15 +37,15 @@ const style = {
 };
 
 function DraggableColorBox(props) {
-    const { classes } = props;
+    const { classes, handleClick, name, color } = props;
 
     return (
         <div className={classes.root} style={{
-            backgroundColor: props.color
+            backgroundColor: color
         }}>
             <div className={classes.boxContent}>
-                <span>{props.name}</span>
-                <DeleteIcon className={classes.deleteIcon}/>
+                <span>{name}</span>
+                <DeleteIcon className={classes.deleteIcon} onClick={handleClick}/>
             </div>
         </div>
     )
--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -144,7 +144,13 @@ class NewPaletteForm extends Component {
 
         this.props.savePalette(newPalette);
         this.props.history.push("/");
-    }
+    };
+
+    removeColor = colorName => {
+        this.setState({
+            colors: this.state.colors.filter( color => color.name !== colorName)
+        });
+    };
 
     render() {
         const { classes } = this.props;
@@ -238,7 +244,11 @@ class NewPaletteForm extends Component {
             >
                 <div className={classes.drawerHeader} />
                 {this.state.colors.map(color => (
-                    <DraggableColorBox key={color} color={color.color} name={color.name}/>
+                    <DraggableColorBox 
+                        key={color.name} 
+                        color={color.color} 
+                        name={color.name} 
+                        handleClick={() => this.removeColor(color.name)}/>
                 ))}
             </main>
         </div>
