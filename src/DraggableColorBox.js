84627db27c9652a65a685a522ebb6788daa8e6a3
Zeztron <pateltharsh@gmail.com>
2019-05-06 20:18:40 -0400

    refactored styles out

--- src/DraggableColorBox.js
+++ src/DraggableColorBox.js
@@ -2,41 +2,7 @@ import React from 'react';
 import { SortableElement } from 'react-sortable-hoc';
 import { withStyles } from '@material-ui/styles';
 import DeleteIcon from '@material-ui/icons/Delete';
-
-const style = {
-    root: {
-        width: "20%",
-        height: "25%",
-        margin: "0 auto",
-        display: "inline-block",
-        position: "relative",
-        cursor: "grab",
-        marginBottom: "-3.5px",
-        "&:hover button": {
-            opacity: "1"
-        },
-        "&:hover svg": {
-            color: "#fff",
-            transform: "scale(1.4)"
-        }
-    },
-    boxContent: {
-        position: "absolute",
-        padding: "10px",
-        width: "100%",
-        left: "0px",
-        bottom: "0px",
-        color: "rgba(0, 0, 0, 0.5)",
-        letterSpacing: "1px",
-        textTransform: "uppercase",
-        fontSize: "12px",
-        display: "flex",
-        justifyContent: "space-between"
-    },
-    deleteIcon: {
-        transition: "all 0.3s ease-in-out"
-    }
-};
+import styles from './styles/DraggableColorBoxStyles';
 
 const DraggableColorBox = SortableElement((props) => {
     const { classes, handleClick, name, color } = props;
@@ -53,4 +19,4 @@ const DraggableColorBox = SortableElement((props) => {
     )
 })
 
-export default withStyles(style)(DraggableColorBox);
+export default withStyles(styles)(DraggableColorBox);
---
+++ src/styles/DraggableColorBoxStyles.js
@@ -0,0 +1,36 @@
+const styles = {
+    root: {
+        width: "20%",
+        height: "25%",
+        margin: "0 auto",
+        display: "inline-block",
+        position: "relative",
+        cursor: "grab",
+        marginBottom: "-3.5px",
+        "&:hover button": {
+            opacity: "1"
+        },
+        "&:hover svg": {
+            color: "#fff",
+            transform: "scale(1.4)"
+        }
+    },
+    boxContent: {
+        position: "absolute",
+        padding: "10px",
+        width: "100%",
+        left: "0px",
+        bottom: "0px",
+        color: "rgba(0, 0, 0, 0.5)",
+        letterSpacing: "1px",
+        textTransform: "uppercase",
+        fontSize: "12px",
+        display: "flex",
+        justifyContent: "space-between"
+    },
+    deleteIcon: {
+        transition: "all 0.3s ease-in-out"
+    }
+};
+
+export default styles;
