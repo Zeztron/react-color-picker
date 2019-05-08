0b6e0a671865246256cfbc6225ff447ef659b080
Zeztron <pateltharsh@gmail.com>
2019-05-08 18:21:50 -0400

    tweaked form styles

--- src/PaletteFormNav.js
+++ src/PaletteFormNav.js
@@ -9,7 +9,7 @@ import Button from '@material-ui/core/Button'
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
+import CreateIcon from '@material-ui/icons/Create';
 import styles from './styles/PaletteFormNavStyles';
 
 class PaletteFormNav extends Component {
@@ -57,7 +57,7 @@ class PaletteFormNav extends Component {
                             onClick={this.props.handleDrawerOpen}
                             className={classNames(classes.menuButton, open && classes.hide)}
                         >
-                            <MenuIcon />
+                            <CreateIcon />
                         </IconButton>
                         <Typography variant="h6" color="inherit" noWrap>
                             Create A Palette
