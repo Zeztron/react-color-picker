219b905a9222ba4787045bb46730dffada4fe9f7
Zeztron <pateltharsh@gmail.com>
2019-05-08 18:31:07 -0400

    save to local storage

--- src/App.js
+++ src/App.js
@@ -6,57 +6,65 @@ import SingleColorPalette from './SingleColorPalette';
 import NewPaletteForm from './NewPaletteForm';
 import seedColors from './seedColor';
 import { generatePalette } from './colorHelpers';
+
+const savedPalettes = JSON.parse(window.localStorage.getItem("palettes"));
+
 class App extends Component {
 
-    state = {
-      palettes: seedColors
-    }
-
-    findPalette = (id) => {
-      return this.state.palettes.find(function(palette) {
-        return palette.id === id;
-      });
-    }
-
-    savePalette = newPalette => {
-      this.setState({
-        palettes: [...this.state.palettes, newPalette]
-      });
-    }
-
-    render() {
-      return (
-        <Switch>
-          <Route 
-            exact 
-            path="/palette/new" 
-            render={(routeProps) => 
-              <NewPaletteForm 
-                savePalette={this.savePalette} 
-                palettes={this.state.palettes} 
-                {...routeProps}
-              />}
-            />
-          <Route 
-            exact 
-            path="/" 
-            render={(routeProps) => 
-              <PaletteList palettes={this.state.palettes} {...routeProps}/>}
-            />
-          <Route 
-            exact 
-            path="/palette/:id" 
-            render={(routeProps) => 
-              <Palette palette={generatePalette(this.findPalette(routeProps.match.params.id))}/>}
-            />
-          <Route 
-            exact 
-            path="/palette/:paletteId/:colorId" 
-            render={(routeProps) => <SingleColorPalette colorId={routeProps.match.params.colorId} palette={generatePalette(this.findPalette(routeProps.match.params.paletteId))} />}
-          />
-        </Switch>
-      );
-    }
+  state = {
+    palettes: savedPalettes || seedColors
   }
+
+  findPalette = (id) => {
+    return this.state.palettes.find(function(palette) {
+      return palette.id === id;
+    });
+  }
+
+  savePalette = newPalette => {
+    this.setState({
+      palettes: [...this.state.palettes, newPalette]
+    }, this.syncLocalStorage);
+  }
+  
+  // save to local storage
+  syncLocalStorage = () => {
+    window.localStorage.setItem("palettes", JSON.stringify(this.state.palettes));
+  }
+
+  render() {
+    return (
+      <Switch>
+        <Route 
+          exact 
+          path="/palette/new" 
+          render={(routeProps) => 
+            <NewPaletteForm 
+              savePalette={this.savePalette} 
+              palettes={this.state.palettes} 
+              {...routeProps}
+            />}
+          />
+        <Route 
+          exact 
+          path="/" 
+          render={(routeProps) => 
+            <PaletteList palettes={this.state.palettes} {...routeProps}/>}
+          />
+        <Route 
+          exact 
+          path="/palette/:id" 
+          render={(routeProps) => 
+            <Palette palette={generatePalette(this.findPalette(routeProps.match.params.id))}/>}
+          />
+        <Route 
+          exact 
+          path="/palette/:paletteId/:colorId" 
+          render={(routeProps) => <SingleColorPalette colorId={routeProps.match.params.colorId} palette={generatePalette(this.findPalette(routeProps.match.params.paletteId))} />}
+        />
+      </Switch>
+    );
+  }
+}
 
 export default App;
